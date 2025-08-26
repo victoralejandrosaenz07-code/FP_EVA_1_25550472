ee8514eca6c4a25dfa5bd6dbe67b93c83f097fd3
victor alejandro saenz herrera <victoralejandrosaenz07@gmail.com>
2025-08-26 00:09:43 -0600

    Add files via upload

---
+++ EVA_1_Hola_Mundo.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package eva_1_hola_mundo;
+
+/**
+ *
+ * @author miria
+ */
+public class EVA_1_Hola_Mundo {
+
+    
+    public static void main(String[] args) {
+        System.out.println("Hello World!!");
+        System.out.println("Fundamentos De Programacion");         
+  }
+}
