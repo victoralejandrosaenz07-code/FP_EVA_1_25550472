4b6d17a1ec1bbe6ea4c0a5dfa6c095cd255a0baf
victor alejandro saenz herrera <victoralejandrosaenz07@gmail.com>
2025-08-28 17:13:56 -0600

    Add files via upload

---
+++ EVA1_5_VARIABLES_25550472/src/eva1_5_variables/EVA1_5_VARIABLES.java
@@ -0,0 +1,34 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package eva1_5_variables;
+
+/**
+ *
+ * @author Alejandro Saenz
+ */
+public class EVA1_5_VARIABLES {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // DECLARAR LAS VARIABLES
+        //TIPON DE DATO   NOMBRE DEL IDENTIFICADOR
+        String nombre;
+        int edad;
+        double salario;
+        //INICIALIZACION:
+        //identificado = valor;
+        //= --> operador de asignacion
+        nombre = "VCTOR ALEJANDRO SAENZ";
+        edad = 43;
+        salario = 1.1;
+        //edad = "VICTOR";
+        System.out.println(nombre);
+        System.out.println(edad);
+        System.out.println(salario);
+    }
+    
+}
