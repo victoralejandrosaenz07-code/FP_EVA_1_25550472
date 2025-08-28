dbfa8881e62c29203b02c3d19fd423d62d926a51
victor alejandro saenz herrera <victoralejandrosaenz07@gmail.com>
2025-08-27 22:21:10 -0600

    Add files via upload

---
+++ EVA1_3_25550472/src/eva1_3_25550472/EVA1_3_25550472.java
@@ -0,0 +1,26 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package eva1_3_25550472;
+
+/**
+ *
+ * @author Alejandro Saenz
+ */
+public class EVA1_3_25550472 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        System.out.print("nombre del estudiante: ");
+        System.out.println("victor saenz");
+        System.out.print("numero de control: ");
+        System.out.println(25550472);
+        System.out.print("edad: ");
+        System.out.println(18);
+    }
+    
+}
