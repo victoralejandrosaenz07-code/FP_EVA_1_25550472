7af67cd45068228f5a7237b94981ac2ff566d69b
victor alejandro saenz herrera <victoralejandrosaenz07@gmail.com>
2025-08-27 20:41:14 -0600

    Add files via upload

---
+++ EVA_1_HOLA MUNDO/src/eva_1_hola/mundo/EVA_1_HOLAMUNDO.java
@@ -0,0 +1,9 @@
+package eva_1_hola.mundo;
+
+public class EVA_1_HOLAMUNDO {
+
+    public static void main(String[] args) {
+         System.out.println("HOLA MUNDO!!");
+         System.out.println("FUNDAMENTOS DE PROGRAMACION");
+    }
+}
