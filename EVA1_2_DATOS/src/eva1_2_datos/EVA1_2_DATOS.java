04a9149c540eb70aec161722203e0345da3f957f
victor alejandro saenz herrera <victoralejandrosaenz07@gmail.com>
2025-08-27 22:19:54 -0600

    Add files via upload

---
+++ EVA1_2_DATOS/src/eva1_2_datos/EVA1_2_DATOS.java
@@ -0,0 +1,35 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package eva1_2_datos;
+
+/**
+ *
+ * @author Alejandro Saenz
+ */
+public class EVA1_2_DATOS {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        //;SIGNIFCA FIN DE LINEA
+        //"   "--  CADENA DE TEXTO.DATO
+        System.out.println("VICTOR SAENZ");
+        //NUMEROS ENTEROS
+        System.out.println(1000);
+        //NUMEROS DECIMALES
+        System.out.println(500.3);
+        //VALORES LOGICOS (VERDADERO O FALSO)
+        //true false
+        System.out.println(true);
+        //CARACTERES INDIVIDUALES
+        //COSAS DISTNTAS
+        System.out.println('c');//CARACTER
+        System.out.println("c");//CADENA DE TEXTO
+                
+    }
+    
+}
