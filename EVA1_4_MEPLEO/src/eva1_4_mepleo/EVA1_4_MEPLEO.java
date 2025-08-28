d2e463474ce02e81433606b58c99cd6a85aed20a
victor alejandro saenz herrera <victoralejandrosaenz07@gmail.com>
2025-08-27 22:22:32 -0600

    Add files via upload

---
+++ EVA1_4_MEPLEO/src/eva1_4_mepleo/EVA1_4_MEPLEO.java
@@ -0,0 +1,30 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package eva1_4_mepleo;
+
+/**
+ *
+ * @author Alejandro Saenz
+ */
+public class EVA1_4_MEPLEO {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        System.out.print("nombre de la empresa: ");
+        System.out.println("pollitos la daly");
+        System.out.print("empleo: ");
+        System.out.println("cosinero");
+        System.out.print("salrio: ");
+        System.out.println("1200 por semana");
+        System.out.print("horas por semana: ");
+        System.out.println("30 horas");
+        System.out.print("requisitos: ");
+        System.out.println("ninguno");
+    }
+    
+}
