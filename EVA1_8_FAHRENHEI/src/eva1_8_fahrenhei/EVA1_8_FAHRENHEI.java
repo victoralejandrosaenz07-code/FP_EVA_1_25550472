00e06bfe4f35596deea353e8a4e94e8175e20d5f
victor alejandro saenz herrera <victoralejandrosaenz07@gmail.com>
2025-09-02 13:11:25 -0600

    Add files via upload

---
+++ EVA1_8_FAHRENHEI/src/eva1_8_fahrenhei/EVA1_8_FAHRENHEI.java
@@ -0,0 +1,28 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package eva1_8_fahrenhei;
+
+/**
+ *
+ * @author Alejandro Saenz
+ */
+public class EVA1_8_FAHRENHEI {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+         double fahrenheit, celsius, total;
+        fahrenheit = 93;
+        celsius = 9*(fahrenheit-55);
+        total = celsius/9;
+        System.out.println("convirtiendo en grados fahrenheit a grados celsius");
+        System.out.println("los grados fahrenheit serian 55");
+        System.out.println("los grados convertidos serian = ");
+        System.out.println(total);
+    }
+    
+}
